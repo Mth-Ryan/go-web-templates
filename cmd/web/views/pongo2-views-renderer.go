b1e88d466ac6502b64c065df1409cae4eee419fd
Mateus Ryan <mthryan@protonmail.com>
2023-10-24 11:33:44 -0300

    Home content and disble template caching for development

--- cmd/web/views/pongo2-views-renderer.go
+++ cmd/web/views/pongo2-views-renderer.go
@@ -1,17 +1,23 @@
 package views
 
 import (
+	"github.com/Mth-Ryan/waveaction/pkg/conf"
 	"github.com/Mth-Ryan/waveaction/pkg/logger"
 	"github.com/flosch/pongo2/v6"
 )
 
 type Pongo2ViewsRenderer struct {
+	appConf       *conf.AppConf
 	logger        logger.ApplicationLogger
 	templateCache map[string](*pongo2.Template)
 }
 
-func NewPongo2ViewsRenderer(logger logger.ApplicationLogger) *Pongo2ViewsRenderer {
+func NewPongo2ViewsRenderer(
+	appConf *conf.AppConf,
+	logger logger.ApplicationLogger,
+) *Pongo2ViewsRenderer {
 	return &Pongo2ViewsRenderer{
+		appConf:       appConf,
 		logger:        logger,
 		templateCache: make(map[string]*pongo2.Template),
 	}
@@ -31,12 +37,14 @@ func (pr *Pongo2ViewsRenderer) Render(
 		if err != nil {
 			return []byte{}, err
 		}
-		
-		pr.logger.Info(
-			"Templ caching",
-			templateFile,
-		)
-		pr.templateCache[templateFile] = pongoTemplate
+
+		if pr.appConf.RunMode == conf.RUN_MODE_RELEASE {
+			pr.logger.Info(
+				"Templ caching",
+				templateFile,
+			)
+			pr.templateCache[templateFile] = pongoTemplate
+		}
 	}
 
 	return pongoTemplate.ExecuteBytes(context)
